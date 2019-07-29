91972d5afd5971cb5c4726f72e33a107464f7380
chener <531479067@qq.com>
2019-07-29 16:26:34 +0800

    create by cs on 20190729

---
+++ app/src/main/java/com/bestom/opencv_javalibrary/MainActivity.java
@@ -0,0 +1,51 @@
+package com.bestom.opencv_javalibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String  TAG= MainActivity.class.getSimpleName();
+
+    //region BaseLoaderCallback OpenCV.loader 的回调函数
+    private BaseLoaderCallback mLoaderCallback=new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    //    surfaceView.setVisibility(View.VISIBLE);
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+    //endregion
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+
+        Mat rgbmat=new Mat();
+
+    }
+}
